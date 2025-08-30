1687f4bbebc2567004a271f88af212509585e5ae
riuzerjc <rayjc.desing@gmail.com>
2025-08-29 23:31:31 -0500

    chore(config): actualiza configuración y dependencias del proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './src/frontend/components/**/*.{js,ts,jsx,tsx}',
+    './src/frontend/app/**/*.{js,ts,jsx,tsx}',
+    './src/frontend/pages/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
