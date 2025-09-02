bdadf7cd89244f8d35944c46e3d6616b5e3ac0f3
riuzerjc <rayjc.desing@gmail.com>
2025-09-01 22:25:58 -0500

    chore(cleanup): remove old API routes

--- src/pages/api/trpc/[trpc].ts
+++
@@ -1,8 +0,0 @@
-import type { AppRouter } from "@backend/trpc/appRouter";
-import { appRouter } from "@backend/trpc/appRouter";
-import { createNextApiHandler } from "@trpc/server/adapters/next";
-
-export default createNextApiHandler({
-  router: appRouter,
-  createContext: ({ req, res }) => ({ req, res }),
-});
